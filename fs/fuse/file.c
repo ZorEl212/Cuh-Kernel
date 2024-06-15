648f4a9da8031e8f82f750f3c31f740e4cfd33dc
ZorEl212 <yabsirad212@proton.me>
2024-06-16 01:20:20 +0300

    Declare variables lost overwritten during cherry-pick
    Signed-off-by: ZorEl212 <yabsirad212@proton.me>

--- fs/fuse/file.c
+++ fs/fuse/file.c
@@ -952,6 +952,7 @@ static ssize_t fuse_file_read_iter(struct kiocb *iocb, struct iov_iter *to)
 	ssize_t ret_val;
 	struct inode *inode = iocb->ki_filp->f_mapping->host;
 	struct fuse_conn *fc = get_fuse_conn(inode);
+	struct fuse_file *ff = iocb->ki_filp->private_data;
 
 	if (fuse_is_bad(inode))
 		return -EIO;
